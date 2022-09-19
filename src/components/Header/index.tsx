05943b52a5956b5170ce16864c34addf346aa74d
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-19 20:20:51 -0300

    feat: implementado o Header e os estilos

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import styles from './styles.module.scss';
+function Header() {
+  const player = useSelector((state: RootState) => state.scoreboard.player);
+  const will = useSelector((state: RootState) => state.scoreboard.will);
+  const draw = useSelector((state: RootState) => state.scoreboard.draw);
+  return (
+    <header className={styles.container}>
+      <h1>Joquempô</h1>
+      <section className={styles.score}>
+        <section>
+          <h3>Player</h3>
+          <p>{player}</p>
+        </section>
+        <section>
+          <h3>Empate</h3>
+          <p>{draw}</p>
+        </section>
+        <section>
+          <h3>Will</h3>
+          <p>{will}</p>
+        </section>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
