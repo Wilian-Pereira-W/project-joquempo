f2ef04677598e493f439ee8283aba8b5a4f772ef
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-19 19:24:03 -0300

    feat: implementado os estilos globais

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
