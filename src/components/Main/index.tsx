21a70586cba3ad8c9b93e4ef0048357bc82e14e7
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-20 11:34:45 -0300

    feat: criando componente Main e os estilos

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,61 @@
+import { dataIcons } from '../../data/icons';
+import Button from '../Button';
+import styles from './styles.module.scss';
+import bgTriangulo from '../../assets/bg-triangle.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { dataIconsSort } from '../../data/iconsSort';
+import Results from '../Results';
+import { RootState } from '../../redux/store';
+import { disable } from '../../features/enableScreen/enableScreenSlice';
+
+export interface Result {
+  id: number;
+  url: string;
+  name: string;
+  cor: string;
+}
+function Main(): JSX.Element {
+  const [playerChoice, setPlayerChoice] = useState<Result[]>([]);
+  const [willChoice, setWillChoice] = useState<Result[]>([]);
+  const [data] = useState(dataIconsSort);
+  const enableScreen = useSelector(
+    (state: RootState) => state.enableScreen.enable,
+  );
+  const dispatch = useDispatch();
+
+  const handleClick = (id: number) => {
+    dispatch(disable());
+    const playerResult = data.filter((icon) => icon.id === id);
+    const sort = data.sort(() => 0.5 - Math.random());
+    const willResult = data.filter((icon) => icon.id === sort[0].id);
+    setPlayerChoice(playerResult);
+    setWillChoice(willResult);
+  };
+  return (
+    <main className={styles.container}>
+      {enableScreen && (
+        <section className={styles.content}>
+          <img
+            className={styles.bgTriangulo}
+            src={bgTriangulo}
+            alt="bgTriangulo"
+          />
+          {dataIcons.map((icon) => (
+            <Button
+              key={icon.id}
+              name={icon.name}
+              url={icon.url}
+              id={icon.id}
+              cor={icon.cor}
+              onClick={() => handleClick(icon.id)}
+            />
+          ))}
+        </section>
+      )}
+      <Results player={playerChoice} will={willChoice} />
+    </main>
+  );
+}
+
+export default Main;
