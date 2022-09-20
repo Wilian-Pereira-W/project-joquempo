35a030ba419c26af2f3e0a612c9eb9359617af91
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-20 16:05:56 -0300

    feat: adicionando enableScoreboard, enableMain e enableResult

--- src/features/enableScreen/enableScreenSlice.ts
+++ src/features/enableScreen/enableScreenSlice.ts
@@ -2,10 +2,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export interface enableScreenState {
   enable: boolean;
+  enableScoreboard: boolean;
+  enableMain: boolean;
+  enableResult: boolean;
 }
 
 const initialState: enableScreenState = {
   enable: true,
+  enableScoreboard: false,
+  enableMain: false,
+  enableResult: false,
 };
 
 export const enableScreenSlice = createSlice({
@@ -18,9 +24,36 @@ export const enableScreenSlice = createSlice({
     disable: (state) => {
       state.enable = false;
     },
+    enableScoreboard: (state) => {
+      state.enableScoreboard = true;
+    },
+    disableScoreboard: (state) => {
+      state.enableScoreboard = false;
+    },
+    enableMain: (state) => {
+      state.enableMain = true;
+    },
+    disableMain: (state) => {
+      state.enableMain = false;
+    },
+    enableResult: (state) => {
+      state.enableResult = true;
+    },
+    disableResult: (state) => {
+      state.enableResult = false;
+    },
   },
 });
 
-export const { enable, disable } = enableScreenSlice.actions;
+export const {
+  enable,
+  disable,
+  enableScoreboard,
+  disableScoreboard,
+  enableMain,
+  disableMain,
+  enableResult,
+  disableResult,
+} = enableScreenSlice.actions;
 
 export default enableScreenSlice.reducer;
