5e97d8bb6c19ea14a914f0023257962988a2fb5a
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-20 11:36:00 -0300

    feat: criando dataIcons

---
+++ src/data/icons.ts
@@ -0,0 +1,24 @@
+import iconPaper from '../assets/icon-paper.svg';
+import iconScissors from '../assets/icon-scissors.svg';
+import iconRock from '../assets/icon-rock.svg';
+
+export const dataIcons = [
+  {
+    id: 1,
+    url: iconPaper,
+    name: 'Paper',
+    cor: 'blue',
+  },
+  {
+    id: 2,
+    url: iconScissors,
+    name: 'Scissors',
+    cor: 'yellow',
+  },
+  {
+    id: 3,
+    url: iconRock,
+    name: 'Rock',
+    cor: 'red',
+  },
+];
