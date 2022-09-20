eb13c1af3542ffe5be619763c2e5293fb1908e6f
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-20 11:39:04 -0300

    feat: implementado enableScreenReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import scoreboardReducer from '../features/scoreboard/scoreboardSlice';
+import enableScreenReducer from '../features/enableScreen/enableScreenSlice';
 
 export const store = configureStore({
   reducer: {
     scoreboard: scoreboardReducer,
+    enableScreen: enableScreenReducer,
   },
 });
 
